c71e9e9a80dd4f117d8e4d9b09ae0bf652340ff8
Andy Le <andyle@Andys-Air.fritz.box>
2023-04-16 18:27:09 +0200

    Add local scope declarations in functions

--- src/Functions.hs
+++ src/Functions.hs
@@ -1,4 +1,11 @@
-module Functions (double, quadruple, factorial, average) where
+module Functions (
+    double, 
+    quadruple, 
+    factorial, 
+    average, 
+    polynomial,
+    sumEvenOdds
+) where
 
 double :: Num a => a -> a
 double x = x + x
@@ -11,3 +18,16 @@ factorial x = product [1 .. x]
 
 average :: (Fractional a, Foldable t) => t a -> a
 average ns = sum ns / fromIntegral (length ns)
+
+polynomial :: Num a => a -> a
+polynomial x = let
+    y = x + 1
+    in y * y
+
+-- sumEvenOdds xs = foldr (+) 0 (map (+ 1) (filter (\x -> x `mod` 2 == 0) xs))
+sumEvenOdds :: Integral b => [b] -> b
+sumEvenOdds xs = sum (incr (even xs))
+    where
+        sum xs = foldr (+) 0 xs
+        incr xs = map (+ 1) xs
+        even xs = filter (\x -> x `mod` 2 == 0) xs
