ea4f2be2c144323257a6de6ab6a4d075178d386b
Andy Le <andyle@Andys-Air.fritz.box>
2023-04-16 14:25:13 +0200

    Added first and last half functions

--- src/Exercise1.hs
+++ src/Exercise1.hs
@@ -1,4 +1,5 @@
-module Exercise1 (last,init,(!!)) where
+{-# OPTIONS_GHC -Wno-identities #-}
+module Exercise1 (last,init,(!!),firstHalf,lastHalf) where
 
 import Prelude hiding (last,init,(!!))
 
@@ -11,3 +12,10 @@ init xs = take (length xs - 1) xs
 (!!) :: [a] -> Int -> a
 xs !! n = last (take (n + 1) xs)
 
+firstHalf :: [a] -> [a]
+firstHalf xs = take (div (length xs + 1) 2) xs
+
+lastHalf :: [a] -> [a]
+lastHalf xs = drop (div (length xs) 2) xs
+-- lastHalf xs = reverse (take (sum [div (length xs) 2, rem (length xs) 2]) (reverse xs))
+-- lastHalf xs = reverse (take (ceiling (toRational (length xs) / 2)) (reverse xs))
