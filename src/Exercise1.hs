5d331ca740b47d97c53907a5e3c197831b1cabdf
Andy Le <andyle@Andys-Air.fritz.box>
2023-04-16 15:19:36 +0200

    Added inners function; refactored index access

--- src/Exercise1.hs
+++ src/Exercise1.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -Wno-identities #-}
-module Exercise1 (last,init,(!!),firstHalf,lastHalf) where
+module Exercise1 (last,init,(!!),firstHalf,lastHalf,inners) where
 
 import Prelude hiding (last,init,(!!))
 
@@ -10,12 +10,18 @@ init :: [a] -> [a]
 init xs = take (length xs - 1) xs
 
 (!!) :: [a] -> Int -> a
-xs !! n = last (take (n + 1) xs)
+xs !! n
+    | n > -1 && n < length xs = last (take (n + 1) xs)
+    | otherwise = error "ERROR"
+
 
 firstHalf :: [a] -> [a]
-firstHalf xs = take (div (length xs + 1) 2) xs
+firstHalf xs = take (div (length xs) 2) xs
 
 lastHalf :: [a] -> [a]
 lastHalf xs = drop (div (length xs) 2) xs
 -- lastHalf xs = reverse (take (sum [div (length xs) 2, rem (length xs) 2]) (reverse xs))
 -- lastHalf xs = reverse (take (ceiling (toRational (length xs) / 2)) (reverse xs))
+
+inners :: [a] -> [a]
+inners xs = init (tail xs)
